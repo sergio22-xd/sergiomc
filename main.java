57903d89606e331d8db63b5956f6ca6cc7d44c9a
sergio22-xd <sergiocadenamejia@gmail.com>
2026-05-04 22:19:47 +0200

    Update main.java

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) {
         saludo();
     }
-//esto es un comentario 
+//esto es un para conflicto
     private static void saludo() {
         System.out.println("Hola mundo");
     }
