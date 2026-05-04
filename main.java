5dc8900f1adc907df647968506836d4c67142c28
sergio22-xd <sergiocadenamejia@gmail.com>
2026-05-04 19:00:29 +0200

    Refactorización aplicada

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
+        saludo();
+    }
+
+    private static void saludo() {
         System.out.println("Hola mundo");
     }
 }
